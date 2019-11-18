3698648f6c78ebdfc1eeefa554340fdf94f0d943
Stephanie Zhang <stephaniezhang@g.harvard.edu>
2019-11-18 18:01:25 -0500

    adding tests for scalar.py and renaming code folder for testing purposes.

---
+++ tests/test_scalar.py
@@ -0,0 +1,68 @@
+import pytest
+import numpy as np
+import sys
+sys.path.append('..')
+import codes.scalar as sc
+
+def test_add():
+    x1 = sc.Scalar(1)
+    x2 = x1 + 4
+    x3 = +x1 + x2 #  TEST__POS__
+    x4 = -x1 + 5 # TEST_NEG__
+    assert x2.val == 5
+    assert x2.der == 1
+    assert x3.val == 6
+    assert x3.der == 2
+    assert x4.val == 4
+    assert x4.der == -1
+
+def test_sub():
+    x1 = sc.Scalar(4)
+    x2 = x1 - 3
+    x3 = x2 - x1
+    x4 = 5 - x1
+    x5 = x1 - 3
+    assert x2.val == 1
+    assert x2.der == 1
+    assert x3.val == -3
+    assert x3.der == 0
+    assert x4.val == 1
+    assert x4.der == -1
+    assert x5.val == 1
+    assert x5.der == 1
+
+def test_mul():
+    x1 = sc.Scalar(5)
+    x2 = 3*x1
+    x3 = x1*x2
+    assert x2.val == 15
+    assert x2.der == 3
+    assert x3.val == 75
+    assert x3.der == 30
+
+def test_div():
+    x1 = sc.Scalar(1)
+    x2 = x1/4
+    x3 = x1/x2
+    x4 = 2/x1
+    assert x2.val == 1/4
+    assert x2.der == 1/4
+    assert x3.val == 4
+    assert x3.der == 0
+    assert x4.val == 2
+    assert x4.der == -2
+
+def test_pow():
+    x1 = sc.Scalar(2)
+    x2 = x1**2
+    x3 = 4**x1 # test __rpow__
+    assert x2.val == 4
+    assert x2.der == 4
+    assert x3.val == 16
+    assert x3.der == np.log(4)*4**2
+
+# test_add()
+# test_sub()
+# test_mul()
+# test_div()
+# test_pow()
