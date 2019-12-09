ec060efde29d6b4b6349da2059cc18cc9f3a038d
William Seaton <wseaton@g.harvard.edu>
2019-12-09 16:44:43 -0500

    Add new values/formulas functions to AD class; Update travis.yml

--- bitterdispute/forward_mode.py
+++ bitterdispute/forward_mode.py
@@ -11,11 +11,15 @@ class AD():
     """
 
     def __init__(self):
+        self.guide()
+
+    def guide(self):
+        """todo
+        """
         print("""
         Welcome to Bitter Dispute! America's favorite automatically
         differentiating gameshow. Let's get started.
         """)
-
         try:
             variable_count = int(input("How many variables are in your formula(s)? The limit is 26. >> "))
         except:
@@ -41,10 +45,10 @@ class AD():
         for keys, values in variable_dict.items():
             print(f"{keys} = {values}")#.val
 
-        print("Lastly, what formulas would you like to derive? Please use the variables just listed.\
-        You may enter as many formulas as you'd like as a list!")
-        # If a list of formulas is entered
+        print("""Lastly, what formulas would you like to derive? Please use the variables just listed.
+        You may enter as many formulas as you'd like, either writing them separated by commas or copying a list.""")
         formulas = input()
+        # If a list of formulas is entered
         if formulas[0] == '[':
             formulas = formulas[1:-1].split(',') # Saves string of formula minus first/last spots representing []
         # If multiple formulas are manually written
@@ -53,8 +57,11 @@ class AD():
         functions = [eval(formula) for formula in formulas]
 
         # Save final outputs to AD class object
-        self.inputs = [f"{keys} = {values.val}" for keys, values in variable_dict.items()]
-        self.values = [function.val for function in functions]
+        inputs = {}
+        for key, value in variable_dict.items():
+            inputs[key] = value.val
+        self.inputs = inputs
+        self.outputs = [function.val for function in functions]
         self.derivatives = [function.der for function in functions]
         self.second_derivatives = [function.der2 for function in functions]
         self.formulas = formulas
@@ -64,13 +71,14 @@ class AD():
         else:
             print(f"The final derivatives of formulas {self.formulas} with your chosen values are {self.derivatives}. Thanks for playing!")
 
+
     def __str__(self):
         """
         Defines what is printed when class is printed
         """
         return f"""
         Formula(s) saved: {self.formulas},
-        Value(s) used: {self.values},
+        Value(s) used: {self.inputs},
         Derivatives found: {self.derivatives}
         """
 
@@ -79,11 +87,59 @@ class AD():
         Allows you to pass in new values with saved formulas
         todo
         """
-        pass
+        variable_dict = {}
+        for variable_key in self.inputs.keys():
+            print(f"Please enter new value for {variable_key}")
+            variable_value = float(input(f"{variable_key} = "))
+            globals()[variable_key] = Variable(variable_key, variable_value)
+            variable_dict[variable_key] = Variable(variable_key, variable_value)
+        print("Thank you, we recorded these values:")
+        for key, value in variable_dict.items():
+            print(f"{key} = {value}")
+
+        functions = [eval(formula) for formula in self.formulas]
+
+        inputs = {}
+        for key, value in variable_dict.items():
+            inputs[key] = value.val
+        self.inputs = inputs
+        self.outputs = [function.val for function in functions]
+        self.derivatives = [function.der for function in functions]
+        self.second_derivatives = [function.der2 for function in functions]
+
+        if len(self.formulas) == 1:
+            print(f"The final derivative of formula {self.formulas} with your chosen values is {self.derivatives}. Thanks for playing!")
+        else:
+            print(f"The final derivatives of formulas {self.formulas} with your chosen values are {self.derivatives}. Thanks for playing!")
 
     def new_formulas(self):
         """
-        Allows you to pass in new formulas with saved values
+        Allows you to pass in new formulas for saved values
         todo
         """
-        pass
+        for variable_key, variable_value in self.inputs.items():
+            globals()[variable_key] = Variable(variable_key, variable_value)
+
+
+        print("""What new formulas would you like to derive? Please use the same saved variables.
+        You may enter as many formulas as you'd like, either writing them separated by commas or copying a list.""")
+        formulas = input()
+        # If a list of formulas is entered
+        if formulas[0] == '[':
+            formulas = formulas[1:-1].split(',') # Saves string of formula minus first/last spots representing []
+        # If multiple formulas are manually written
+        else:
+            formulas = formulas.split(',') # Saves string of formula split on comma to make list
+
+        functions = [eval(formula) for formula in formulas]
+
+        # Save final outputs to AD class object
+        self.outputs = [function.val for function in functions]
+        self.derivatives = [function.der for function in functions]
+        self.second_derivatives = [function.der2 for function in functions]
+        self.formulas = formulas
+
+        if len(formulas) == 1:
+            print(f"The final derivative of formula {self.formulas} with your chosen values is {self.derivatives}. Thanks for playing!")
+        else:
+            print(f"The final derivatives of formulas {self.formulas} with your chosen values are {self.derivatives}. Thanks for playing!")
