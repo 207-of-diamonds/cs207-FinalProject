fc1a32ac9c16e562fb7cdce488a6df60e34418ea
Stephanie Zhang <stephaniezhang@g.harvard.edu>
2019-12-10 09:55:10 -0500

    editting tests for second order derivative.

--- tests/test_elementary_functions.py
+++ tests/test_elementary_functions.py
@@ -12,7 +12,7 @@ def test_sin_result_var():
     x = Variable('x', val)
     obj_generate = elem.sin(x)
     obj_wanted = Variable(name='x', value=np.sin(val), 
-                          derivative=np.cos(val), second_derivative=0)
+                          derivative=np.cos(val), second_derivative=-np.sin(val))
     
     assert np.isclose(obj_generate.val, obj_wanted.val)
 
@@ -34,7 +34,7 @@ def test_tan_result_var():
     val = 1
     x = Variable('x', val)
     obj_generate = elem.tan(x)
-    obj_wanted = Variable(name='x', value=np.tan(val), derivative=1/(np.cos(val))**2, second_derivative=0)
+    obj_wanted = Variable(name='x', value=np.tan(val), derivative=1/(np.cos(val))**2, second_derivative=2*np.tan(x)*(1/np.cos(x))**2)
     assert np.isclose(obj_generate.val, obj_wanted.val)
     assert list(obj_generate.der) == list(obj_wanted.der)
     assert sum(list(map(lambda x: not np.isclose(obj_generate.der[x], obj_wanted.der[x]), obj_generate.der)))==0
@@ -51,7 +51,7 @@ def test_log_result_var():
     val = 1
     x = Variable('x', val)
     obj_generate = elem.log(x)
-    obj_wanted = Variable(name='x', value=np.log(val), derivative=1/(val), second_derivative=0)
+    obj_wanted = Variable(name='x', value=np.log(val), derivative=1/(val), second_derivative=-1/(val)**2)
     assert np.isclose(obj_generate.val, obj_wanted.val)
     assert list(obj_generate.der) == list(obj_wanted.der)
     assert sum(list(map(lambda x: not np.isclose(obj_generate.der[x], obj_wanted.der[x]), obj_generate.der)))==0
@@ -75,7 +75,7 @@ def test_sqrt_result_var():
     val = 4
     x = Variable('x', val)
     obj_generate = elem.sqrt(x)
-    obj_wanted = Variable(name='x', value=np.sqrt(val), derivative=1/(2*np.sqrt(x.val)), second_derivative=0)
+    obj_wanted = Variable(name='x', value=np.sqrt(val), derivative=1/(2*np.sqrt(x.val)), second_derivative=-1/(4*val**(3/2)))
     assert np.isclose(obj_generate.val, obj_wanted.val)
     assert list(obj_generate.der) == list(obj_wanted.der)
     assert sum(list(map(lambda x: not np.isclose(obj_generate.der[x], obj_wanted.der[x]), obj_generate.der)))==0
@@ -109,7 +109,7 @@ def test_arcsin_result_var():
     val = 0
     x = Variable('x', val)
     obj_generate = elem.arcsin(x)
-    obj_wanted = Variable('x', np.arcsin(val), 1/np.sqrt(1-val**2))
+    obj_wanted = Variable('x', np.arcsin(val), 1/np.sqrt(1-val**2), val/(1-val**2)**(3/2))
     assert np.isclose(obj_generate.val, obj_wanted.val)
     ## ensure same element
     assert list(obj_generate.der)==list(obj_wanted.der)  
@@ -141,7 +141,7 @@ def test_arccos_result_var():
     val = 0
     x = Variable('x',val)
     obj_generate = elem.arccos(x)
-    obj_wanted = Variable('x',np.arccos(val), -1/np.sqrt(1-val**2))
+    obj_wanted = Variable('x',np.arccos(val), -1/np.sqrt(1-val**2), -val/(1-val**2)**(3/2))
     assert np.isclose(obj_generate.val, obj_wanted.val)
     ## ensure same element
     assert list(obj_generate.der)==list(obj_wanted.der)  
@@ -168,7 +168,7 @@ def test_arctan_result_var():
     val = 1
     x = Variable('x', val)
     obj_generate = elem.arctan(x)
-    obj_wanted = Variable('x', np.arctan(val), 1/(val**2+1))
+    obj_wanted = Variable('x', np.arctan(val), 1/(val**2+1), -2*val/(val**2+1)**2)
     assert np.isclose(obj_generate.val, obj_wanted.val)
     ## ensure same element
     assert list(obj_generate.der)==list(obj_wanted.der)  
@@ -188,7 +188,7 @@ def test_exp_result_var():
     val = 1
     x = Variable('x', val)
     obj_generate = elem.exp(x)
-    obj_wanted = Variable('x', np.exp(val), np.exp(val))
+    obj_wanted = Variable('x', np.exp(val), np.exp(val),np.exp(val))
     assert np.isclose(obj_generate.val, obj_wanted.val)
     ## ensure same element
     assert list(obj_generate.der)==list(obj_wanted.der)  
@@ -210,7 +210,7 @@ def test_sihn_result_var():
     x = Variable('x',val)
     obj_generate = elem.sinh(x)
     obj_wanted = Variable(name='x', value = np.sinh(val), 
-                          derivative = np.cosh(val), second_derivative=0)
+                          derivative = np.cosh(val), second_derivative=np.sinh(val))
     assert np.isclose(obj_generate.val, obj_wanted.val)
     ## ensure same element
     assert list(obj_generate.der)==list(obj_wanted.der)  
@@ -230,7 +230,7 @@ def test_cosh_result_var():
     x = Variable('x',val)
     obj_generate = elem.cosh(x)
     obj_wanted = Variable(name='x', value= np.cosh(val), 
-                          derivative = np.sinh(val), second_derivative=0)
+                          derivative = np.sinh(val), second_derivative=np.cosh(val))
     assert np.isclose(obj_generate.val, obj_wanted.val)
     ## ensure same element
     assert list(obj_generate.der)==list(obj_wanted.der)  
@@ -250,7 +250,7 @@ def test_tanh_result_var():
     x = Variable('x',val)
     obj_generate = elem.tanh(x)
     obj_wanted = Variable(name='x', value=np.tanh(val), 
-                          derivative = (1/np.cosh(val))**2, second_derivative=0)
+                          derivative = (1/np.cosh(val))**2, second_derivative=-2*sinh(val)/(cosh(val))**3)
     assert np.isclose(obj_generate.val, obj_wanted.val)
     ## ensure same element
     assert list(obj_generate.der)==list(obj_wanted.der)  
