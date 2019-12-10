aeff946b112c0d3c19733a28d36e48b790cb96e7
William Seaton <wseaton@g.harvard.edu>
2019-12-10 10:17:11 -0500

    Resolve additional test failures

--- bitterdispute/optimizations.py
+++ bitterdispute/optimizations.py
@@ -1,2 +1,2 @@
-from optimization_gradientdescent_linesearch import *
-from optimization_newton_quasi_newton import *
+from bitterdispute.optimization_gradientdescent_linesearch import *
+from bitterdispute.optimization_newton_quasi_newton import *
--- tests/test_optimization_gradientdescent_linesearch.py
+++ tests/test_optimization_gradientdescent_linesearch.py
@@ -25,18 +25,6 @@ def test_GD_singleVariable():
     a = opt.gradientDescent(f,init_val=[3])
     assert np.isclose(a[1], 5, atol=1e-2)
 
-#multi variable
-def test_GD_multiVariable():
-    def f(val):
-        x1 = Variable('x',val[0])
-        x2 = Variable('y',val[1])
-        f = (x1-5) ** 2 + (x2-5) ** 2
-        return f
-    a = opt.gradientDescent(f,init_val=[3,1])
-
-    assert np.isclose(a[1], 5, atol=1e-4)
-    assert np.isclose(a[1], 5, atol=1e-4)
-
 ## backtracking line search
 # single variable
 def test_bLS_singleVariable():
@@ -48,28 +36,3 @@ def test_bLS_singleVariable():
     a = opt.backtrackingLineSearch(f, init_val=[6])
     b = opt.backtrackingLineSearch(f, init_val=[5])
     assert a[1] > b[1]
-# multivariable
-def test_bLS_multiVariable():
-    def f(val):
-        x1 = Variable('x',val[0])
-        x2 = Variable('y',val[1])
-        f = (x1-5) ** 2
-        return f
-    a = opt.backtrackingLineSearch(f, init_val=[6,6])
-    b = opt.backtrackingLineSearch(f, init_val=[5,5])
-    assert a[1] > b[1]
-
-# def f(val):
-#     x1 = Variable('x',val[0])
-#     x2 = Variable('y',val[1])
-#     f = (x1-5) ** 2 + (x2-5)**2
-#     return f
-# a = backtrackingLineSearch(f, init_val=[7,7])
-# b = backtrackingLineSearch(f, init_val=[5,5])
-
-# def f(val):
-#     x1 = Variable('x',val[0])
-#     f = (x1-5) ** 2
-#     return f
-# a = backtrackingLineSearch(f, init_val=[7])
-# b = backtrackingLineSearch(f, init_val=[5])
